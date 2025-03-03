3f7398927689495a6d4908688efae9366f0865fe
steph-leo <leostephs89@gmail.com>
2025-03-03 12:52:52 +0300

    add files

---
+++ Basics/lusagana.js
@@ -0,0 +1,24 @@
+const EXPECTED_MINUTES_IN_OVEN = 40;
+const PREPARATION_MINUTES_PER_LAYER = 2;
+
+function remainingMinutesInOven(actualMinutesInOven) {
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+}
+
+function preparationTimeInMinutes(numberOfLayers) {
+  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER;
+}
+
+function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+}
+
+// Export functions using CommonJS
+module.exports = {
+  remainingMinutesInOven,
+  preparationTimeInMinutes,
+  totalTimeInMinutes
+};
+console.log(remainingMinutesInOven(30));  
+console.log(preparationTimeInMinutes(3));
+console.log(totalTimeInMinutes(3, 30));
